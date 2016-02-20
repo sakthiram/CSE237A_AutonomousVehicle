8aea5df774527a096ec3b34f19d673f475a89361
Debjit Roy <debjitroy1992@gmail.com>
2016-02-20 14:55:29 -0800

    first week files

---
+++ week1/irled.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SIGPIN 2
+
+int main (void)
+{
+struct timeval tv,tve;
+struct timezone tz;
+
+printf (“RasPi Distance with HC-SR04 \n”) ;
+
+if (wiringPiSetup () == -1)
+exit (1) ;
+
+pinMode (ECHOPIN, INPUT) ;
+pinMode (TRIGPIN, OUTPUT) ;
+
+digitalWrite (TRIGPIN,0) ; //LOW
+delayMicroseconds(2);
+digitalWrite (TRIGPIN,1) ; //HIGH
+delayMicroseconds(10);
+digitalWrite (TRIGPIN,0) ; //LOW
+for(;;) {
+if (digitalRead (ECHOPIN) == 1) {
+gettimeofday(&tv,&tz);
+break;
+}
+}
+for(;;) {
+if (digitalRead (ECHOPIN) == 0) {
+gettimeofday(&tve,&tz);
+break;
+}
+}
+
+float usec;
+usec = tve.tv_usec – tv.tv_usec;
+usec = usec/58; //Distance = ((Duration of high level)*(Sonic :340m/s))/2
+printf(“%f\n”,usec);
+return 0 ;
+}
